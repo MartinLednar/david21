525f973685be54aa1b33e495e96077743289eb38
Martin Lednar <martin.lednar03@gmail.com>
2021-11-24 17:44:56 +0100

    Final commit

--- server.js
+++ server.js
@@ -4,15 +4,26 @@ const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 const app = require('./app');
 
-mongoose
-  .connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection successful');
-  });
+if (process.env.NODE_ENV === 'production') {
+  mongoose
+    .connect(process.env.DATABASE, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connection successful');
+    });
+} else {
+  mongoose
+    .connect(process.env.DATABASE_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connection successful');
+    });
+}
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}...`);
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`Server running on port ${process.env.PORT || 5000}...`);
 });
