0ea6acd3d65e761ffd78d0e9836056ad01707882
Martin Lednar <martin.lednar03@gmail.com>
2021-11-24 19:26:40 +0100

    Heroku oprava

--- server.js
+++ server.js
@@ -13,15 +13,6 @@ if (process.env.NODE_ENV === 'production') {
     .then(() => {
       console.log('DB Atlas connection successful');
     });
-} else {
-  mongoose
-    .connect(process.env.DATABASE_LOCAL, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
-    .then(() => {
-      console.log('DB connection successful');
-    });
 }
 
 app.listen(process.env.PORT || 5000, () => {
