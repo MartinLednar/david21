94976b4b8f7174aabba7c436b93c9bad19c47ead
Martin Lednar <martin.lednar03@gmail.com>
2021-11-25 10:13:57 +0100

    Heroku oprava

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(cookieParser());
 app.use(
   session({
     store: MongoStore.create({
-      mongoUrl: process.env.DATABASE_LOCAL,
+      mongoUrl: process.env.DATABASE,
       ttl: 14 * 24 * 60 * 60, // =  expiration
       autoRemove: 'disabled',
       autoRemoveInterval: 15,
--- server.js
+++ server.js
@@ -13,15 +13,16 @@ if (process.env?.NODE_ENV) {
     .then(() => {
       console.log('DB Atlas connection successful');
     });
+} else {
+  mongoose
+    .connect(process.env.DATABASE_LOCAL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log('DB connection successful');
+    });
 }
-mongoose
-  .connect(process.env.DATABASE_LOCAL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log('DB connection successful');
-  });
 
 app.listen(process.env.PORT || 5000, () => {
   console.log(`Server running on port ${process.env.PORT || 5000}...`);
