afd57a83fc67a6a2117812ce1ab37315d43844cc
Martin Lednar <martin.lednar03@gmail.com>
2021-11-30 15:12:50 +0100

    Opravene maily

--- utils/email.js
+++ utils/email.js
@@ -2,37 +2,38 @@ const nodemailer = require('nodemailer');
 const ejs = require('ejs');
 
 //1 Create transporter
-// if (process.env.NODE_ENV) {
-const transporter = nodemailer.createTransport({
-  host: 'smtp.gmail.com',
-  port: 465,
-  secure: true,
-  auth: {
-    type: 'OAuth2',
-    user: process.env.CLIENT_EMAIL_PROD,
-    clientId: process.env.CLIENT_ID_PROD,
-    clientSecret: process.env.CLIENT_SECRET_PROD,
-    refreshToken: process.env.REFRESH_TOKEN_PROD,
-    accessToken: process.env.ACCESS_TOKEN_PROD,
-    expires: 1484314697598,
-  },
-});
-// }
+if (process.env.NODE_ENV) {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: process.env.CLIENT_EMAIL_PROD,
+      clientId: process.env.CLIENT_ID_PROD,
+      clientSecret: process.env.CLIENT_SECRET_PROD,
+      refreshToken: process.env.REFRESH_TOKEN_PROD,
+      accessToken: process.env.ACCESS_TOKEN_PROD,
+      expires: 1484314697598,
+    },
+  });
+} else {
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      type: 'OAuth2',
+      user: process.env.CLIENT_EMAIL,
+      clientId: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      refreshToken: process.env.REFRESH_TOKEN,
+      accessToken: process.env.ACCESS_TOKEN,
+      expires: 1484314697598,
+    },
+  });
+}
 
-// const transporter = nodemailer.createTransport({
-//   host: 'smtp.gmail.com',
-//   port: 465,
-//   secure: true,
-//   auth: {
-//     type: 'OAuth2',
-//     user: process.env.CLIENT_EMAIL,
-//     clientId: process.env.CLIENT_ID,
-//     clientSecret: process.env.CLIENT_SECRET,
-//     refreshToken: process.env.REFRESH_TOKEN,
-//     accessToken: process.env.ACCESS_TOKEN,
-//     expires: 1484314697598,
-//   },
-// });
 ///////
 
 exports.sendEmailInShop = async options => {
