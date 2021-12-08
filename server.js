875b25cb7b9f33246f1cf3b3a7c2a2062b52571c
Martin Lednar <martin.lednar03@gmail.com>
2021-12-08 18:02:25 +0100

    Oprava loadovanie pesniciek

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ if (process.env?.NODE_ENV) {
     .connect(process.env.DATABASE, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      minPoolSize: 100,
     })
     .then(() => {
       console.log('DB Atlas connection successful');
@@ -18,6 +19,7 @@ if (process.env?.NODE_ENV) {
     .connect(process.env.DATABASE_LOCAL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      minPoolSize: 100,
     })
     .then(() => {
       console.log('DB connection successful');
